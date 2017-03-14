cad7e9265ceb26f0c4bad7fae78b7c6b6dfde6e8
Carter Bullard <carter@qosient.com>
2017-03-14 18:04:12 -0400

    Rasqlinsert bug fix for not aggregating records when writing to a single table.
    Move allocation of parser->RaBinsProcess, so that its only allocated when we see a split mode on the command line.

--- clients/ra.c
+++ clients/ra.c
@@ -65,8 +65,7 @@ extern int ArgusTotalFarRecords;
 
 extern struct ArgusParserStruct *ArgusParser;
 
-int RaPrintCounter = 0;
-
+int RaPrintCounter = 1;
 
 void
 ArgusClientInit (struct ArgusParserStruct *parser)
@@ -480,7 +479,6 @@ RaProcessThisRecord (struct ArgusParserStruct *parser, struct ArgusRecordStruct 
                }
       
                bzero (buf, sizeof(buf));
-               argus->rank = RaPrintCounter++;
 
                if ((ArgusParser->eNoflag == 0 ) || ((ArgusParser->eNoflag >= argus->rank) && (ArgusParser->sNoflag <= argus->rank))) {
                   ArgusPrintRecord(parser, buf, argus, MAXSTRLEN);

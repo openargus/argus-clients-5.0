openargus/argus-clients-5.0

commit af01ff87de445bc47415e24dc107a60052cf1c73

simplify ArgusWriteSocket() full queue handling
ArgusWriteOutSocket() is always called immediately after
ArgusWriteSocket(), so do not call _from_ ArgusWriteSocket().

Remove however many items from the socket queue needed to get the
length down to the maximum and then add the new item.  A later call
to ArgusWriteOutSocket can then determine if the socket should be
closed.  In other words, ArgusWriteOutSocket was unable to send a
complete flow record and the queue remains full.

Make ArgusMaxListLength static const.  It never changes and isn't
used outside of argus_output.c.

Correct the way the socket queue is freed; it holds ArgusQueueNode
structures.

don't need ARGUS_MAX_TOSS_RECORD

Trac ticket #75

common/argus_output.c
@@ -55,7 +55,6 @@
 #include <argus_util.h>
 #include <argus_output.h>
 
-
 void ArgusSendFile (struct ArgusOutputStruct *, struct ArgusClientData *, char *, int);
 static void *ArgusControlChannelProcess(void *);
 
@@ -2578,7 +2577,7 @@ ArgusDeleteSocket (struct ArgusOutputStruct *output, struct ArgusClientData *cli
 #define ARGUS_MAXERROR		500000
 #define ARGUS_MAXWRITENUM	64
 
-int ArgusMaxListLength = 500000;
+static const int ArgusMaxListLength = 500000;
 int ArgusCloseFile = 0;
 
 
@@ -2595,25 +2594,36 @@ ArgusWriteSocket(struct ArgusOutputStruct *output,
    struct ArgusListStruct *list = asock->ArgusOutputList;
    struct ArgusQueueNode *node;
 
-   if (list->count >= ArgusMaxListLength) {
-      if (ArgusWriteOutSocket(output, client) < 0) {
-#if defined(ARGUS_THREADS)
-         pthread_mutex_lock(&list->lock);
-#endif
-         if (list->count >= ArgusMaxListLength) {
-            struct ArgusWireFmtBuffer *tawf;
-            int i;
-#define ARGUS_MAX_TOSS_RECORD	128
-            ArgusLog (LOG_WARNING, "ArgusWriteSocket: tossing records to %s\n", client->hostname);
+   if (list->count > ArgusMaxListLength) {
+      struct ArgusWireFmtBuffer *tawf;
+      int i = 0;
 
-            for (i = 0; i < ARGUS_MAX_TOSS_RECORD; i++)
-               if ((tawf = (struct ArgusWireFmtBuffer *)ArgusPopFrontList(list, ARGUS_NOLOCK)) != NULL)
-                  FreeArgusWireFmtBuffer(tawf);
-         }
 #if defined(ARGUS_THREADS)
-         pthread_mutex_unlock(&list->lock);
+      pthread_mutex_lock(&list->lock);
 #endif
+
+      /* Get the queue length down to the max.  The addition below
+       * will push it back over the max length and ArgusWriteOutSocket()
+       * can later determine if it needs to hang up the connection.
+       */
+      while (list->count > ArgusMaxListLength) {
+         node = (struct ArgusWireFmtBuffer *)ArgusPopFrontList(list, ARGUS_NOLOCK);
+         if (node == NULL)
+            break;
+
+         tawf = node->datum;
+         FreeArgusQueueNode(node);
+         FreeArgusWireFmtBuffer(tawf);
+         i++;
       }
+
+      ArgusLog(LOG_WARNING, "%s: tossed %d record(s) for slow client %s\n",
+               __func__, i, client->hostname);
+
+
+#if defined(ARGUS_THREADS)
+      pthread_mutex_unlock(&list->lock);
+#endif
    }
 
 #ifdef ARGUSDEBUG

commit 577f71fc17923978cf59f667520b4662669d6d46

cast queue element to correct type in ArgusWriteSocket
Fix typecast when cleaning up a client output queue.

Fixes: af01ff87de44 ("simplify ArgusWriteSocket() full queue handling")

common/argus_output.c
@@ -2613,7 +2613,7 @@ ArgusWriteSocket(struct ArgusOutputStruct *output,
        * can later determine if it needs to hang up the connection.
        */
       while (list->count > ArgusMaxListLength) {
-         node = (struct ArgusWireFmtBuffer *)ArgusPopFrontList(list, ARGUS_NOLOCK);
+         node = (struct ArgusQueueNode *)ArgusPopFrontList(list, ARGUS_NOLOCK);
          if (node == NULL)
             break;
 
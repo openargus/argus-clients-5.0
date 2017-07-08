openargus/argus-clients-5.0

commit bcb655f56523371f9a6709c166ffd59b1dd63b27

ArgusEstablishListen() takes output structure parameter
This is a step toward separating the acceptance of new client and
control connections into a separate thread.  ArgusEstablishListen takes
an ArgusOutputStruct and associates it with a particular listening file
descriptor.  It is possible to have multiple listening file descriptors
for a single output.

Trac ticket #77

common/argus_output.c
@@ -250,7 +250,9 @@ RadiumDNSServiceCallback (DNSServiceRef sdRef, DNSServiceFlags flags, DNSService
 #endif
 
 int
-ArgusEstablishListen (struct ArgusParserStruct *parser, int port, char *baddr)
+ArgusEstablishListen (struct ArgusParserStruct *parser,
+                      struct ArgusOutputStruct *output,
+                      int port, char *baddr)
 {
    int s = -1;
 
@@ -304,7 +306,12 @@ ArgusEstablishListen (struct ArgusParserStruct *parser, int port, char *baddr)
                   setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
                   if (!(bind (s, host->ai_addr, host->ai_addrlen))) {
                      if ((retn = listen (s, ARGUS_MAXLISTEN)) >= 0) {
-                        parser->ArgusLfd[parser->ArgusListens++] = s;
+                        parser->ArgusOutputs[parser->ArgusListens] = output;
+                        parser->ArgusLfd[parser->ArgusListens] = s;
+                        parser->ArgusListens++;
+
+                        output->ArgusLfd[output->ArgusListens] = s;
+                        output->ArgusListens++;
                      } else {
                         ArgusLog(LOG_ERR, "ArgusEstablishListen: listen() error %s", strerror(errno));
                      }
@@ -355,7 +362,12 @@ ArgusEstablishListen (struct ArgusParserStruct *parser, int port, char *baddr)
             setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
             if (!(bind (s, (struct sockaddr *)&sin, sizeof(sin)))) {
                if ((listen (s, ARGUS_MAXLISTEN)) >= 0) {
-                  parser->ArgusLfd[parser->ArgusListens++] = s;
+                  parser->ArgusOutputs[parser->ArgusListens] = output;
+                  parser->ArgusLfd[parser->ArgusListens] = s;
+                  parser->ArgusListens++;
+
+                  output->ArgusLfd[output->ArgusListens] = s;
+                  output->ArgusListens++;
                } else {
                   close (s);
                   s = -1;

clients/radium.c
@@ -173,11 +173,6 @@ ArgusClientInit (struct ArgusParserStruct *parser)
          }
       }
 
-      if (parser->ArgusPortNum != 0) {
-         if (ArgusEstablishListen (parser, parser->ArgusPortNum, parser->ArgusBindAddr) < 0)
-            ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
-      }
-
       if (chroot_dir != NULL)
          ArgusSetChroot(chroot_dir);
  
@@ -220,6 +215,13 @@ ArgusClientInit (struct ArgusParserStruct *parser)
                                                 RadiumMaxSsf,
                                                 RadiumAuthLocalhost)) == NULL)
          ArgusLog (LOG_ERR, "could not create output: %s\n", strerror(errno));
+
+      /* Need valid parser->ArgusOutput before starting listener */
+      if (parser->ArgusPortNum != 0) {
+         if (ArgusEstablishListen (parser, parser->ArgusOutput,
+                                   parser->ArgusPortNum, parser->ArgusBindAddr) < 0)
+            ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
+      }
 
 #if defined(ARGUS_THREADS)
       sigemptyset(&blocked_signals);

common/argus_parser.c
@@ -149,8 +149,10 @@ ArgusInitializeParser(struct ArgusParserStruct *parser)
 
    parser->ArgusGenerateManRecords = 0;
 
-   for (i = 0; i < ARGUS_MAXLISTEN; i++)
+   for (i = 0; i < ARGUS_MAXLISTEN; i++) {
       parser->ArgusLfd[i] = -1;
+      parser->ArgusOutputs[i] = NULL;
+   }
 
    parser->ArgusListens = 0;
 

examples/radhcp/radhcp.c
@@ -250,11 +250,12 @@ ArgusClientInit (struct ArgusParserStruct *parser)
       if (parser->ArgusControlPort != 0) {
          struct timeval *tvp = getArgusMarReportInterval(ArgusParser);
 
-         if (ArgusEstablishListen (parser, parser->ArgusControlPort, "127.0.0.1") < 0)
-            ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
-
          if ((parser->ArgusControlChannel = ArgusNewControlChannel (parser)) == NULL)
             ArgusLog (LOG_ERR, "could not create control channel: %s\n", strerror(errno));
+
+         if (ArgusEstablishListen (parser, parser->ArgusControlChannel,
+                                   parser->ArgusControlPort, "127.0.0.1") < 0)
+            ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
 
          if ((tvp->tv_sec == 0) && (tvp->tv_usec == 0)) {
             setArgusMarReportInterval (ArgusParser, "60s");

examples/radns/radns.c
@@ -672,11 +672,12 @@ ArgusClientInit (struct ArgusParserStruct *parser)
       if (parser->ArgusControlPort != 0) {
          struct timeval *tvp = getArgusMarReportInterval(ArgusParser);
 
-         if (ArgusEstablishListen (parser, parser->ArgusControlPort, "127.0.0.1") < 0)
-            ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
-
          if ((parser->ArgusControlChannel = ArgusNewControlChannel (parser)) == NULL)
             ArgusLog (LOG_ERR, "could not create control channel: %s\n", strerror(errno));
+
+         if (ArgusEstablishListen (parser, parser->ArgusControlChannel,
+                                   parser->ArgusControlPort, "127.0.0.1") < 0)
+            ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
 
          if ((tvp->tv_sec == 0) && (tvp->tv_usec == 0)) {
             setArgusMarReportInterval (ArgusParser, "60s");

examples/rampc/rampcd.c
@@ -297,11 +297,6 @@ ArgusClientInit (struct ArgusParserStruct *parser)
          }
       }
 
-      if (parser->ArgusPortNum != 0) {
-         if (ArgusEstablishListen (parser, parser->ArgusPortNum, parser->ArgusBindAddr) < 0)
-            ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
-      }
-
       if (chroot_dir != NULL)
          ArgusSetChroot(chroot_dir);
  
@@ -337,6 +332,12 @@ ArgusClientInit (struct ArgusParserStruct *parser)
                                                 RadiumMaxSsf,
                                                 RadiumAuthLocalhost)) == NULL)
          ArgusLog (LOG_ERR, "could not generate output: %s.\n", strerror(errno));
+
+      if (parser->ArgusPortNum != 0) {
+         if (ArgusEstablishListen (parser, parser->ArgusOutput,
+                                   parser->ArgusPortNum, parser->ArgusBindAddr) < 0)
+            ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
+      }
 
       tvp = getArgusMarReportInterval(ArgusParser);
       if ((tvp->tv_sec == 0) && (tvp->tv_usec == 0)) {

examples/ramysql/raclient.c
@@ -929,11 +929,12 @@ ArgusClientInit (struct ArgusParserStruct *parser)
 
 /*
          if (parser->ArgusControlPort != 0) {
-            if (ArgusEstablishListen (parser, parser->ArgusControlPort, "127.0.0.1") < 0)
-               ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
-
             if ((parser->ArgusControlChannel = ArgusNewControlChannel (parser)) == NULL)
                ArgusLog (LOG_ERR, "could not create control channel: %s\n", strerror(errno));
+
+            if (ArgusEstablishListen (parser, parser->ArgusControlChannel,
+                                      parser->ArgusControlPort, "127.0.0.1") < 0)
+               ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
 
             tvp = getArgusMarReportInterval(ArgusParser);
             if ((tvp->tv_sec == 0) && (tvp->tv_usec == 0)) {

examples/ratop/raclient.c
@@ -927,11 +927,12 @@ ArgusClientInit (struct ArgusParserStruct *parser)
          }
 
          if (parser->ArgusControlPort != 0) {
-            if (ArgusEstablishListen (parser, parser->ArgusControlPort, "127.0.0.1") < 0)
-               ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
-
             if ((parser->ArgusControlChannel = ArgusNewControlChannel (parser)) == NULL)
                ArgusLog (LOG_ERR, "could not create control channel: %s\n", strerror(errno));
+
+            if (ArgusEstablishListen (parser, parser->ArgusControlChannel,
+                                      parser->ArgusControlPort, "127.0.0.1") < 0)
+               ArgusLog (LOG_ERR, "setArgusPortNum: ArgusEstablishListen returned %s", strerror(errno));
 
             tvp = getArgusMarReportInterval(ArgusParser);
             if ((tvp->tv_sec == 0) && (tvp->tv_usec == 0)) {

commit 602f652c262dc7399a9686b9fe062ea91cd8a692

lock access to parser struct Lfd* fields
Protect these fields with the parser lock.
Data race found with Valgrind:

==32600== Possible data race during write of size 4 at 0x66D643C by thread #1
==32600== Locks held: none
==32600==    at 0x4B4513: ArgusEstablishListen (argus_output.c:327)
==32600==    by 0x40511E: ArgusClientInit (radium.c:229)
==32600==    by 0x407576: main (argus_main.c:193)
==32600==
==32600== This conflicts with a previous read of size 4 by thread #3
==32600== Locks held: 1, at address 0x65D62A0
==32600==    at 0x4BCB45: __build_output_array (argus_output.c:3362)
==32600==    by 0x4BCBD7: ArgusListenProcess (argus_output.c:3401)
==32600==    by 0x4C3083E: mythread_wrapper (hg_intercepts.c:389)
==32600==    by 0x5361DC4: start_thread (in /usr/lib64/libpthread-2.17.so)
==32600==    by 0x5AB2CEC: clone (in /usr/lib64/libc-2.17.so)
==32600==  Address 0x66d643c is 1,049,596 bytes inside a block of size 1,325,120 alloc'd
==32600==    at 0x4C2CD95: calloc (vg_replace_malloc.c:711)
==32600==    by 0x445B14: __calloc (argus_util.c:24463)
==32600==    by 0x445A7B: __argus_malloc (argus_util.c:24399)
==32600==    by 0x445B42: ArgusCalloc (argus_util.c:24470)
==32600==    by 0x454FC7: ArgusNewParser (argus_parser.c:89)
==32600==    by 0x40729D: main (argus_main.c:133)
==32600==  Block was alloc'd by thread #1

Fixes: bcb655f56523 ("ArgusEstablishListen() takes output structure parameter")

common/argus_output.c
@@ -321,10 +321,12 @@ ArgusEstablishListen (struct ArgusParserStruct *parser,
                   setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
                   if (!(bind (s, host->ai_addr, host->ai_addrlen))) {
                      if ((retn = listen (s, ARGUS_LISTEN_BACKLOG)) >= 0) {
+                        MUTEX_LOCK(&parser->lock);
                         parser->ArgusOutputs[parser->ArgusListens] = output;
                         parser->ArgusLfdVersion[parser->ArgusListens] = version;
                         parser->ArgusLfd[parser->ArgusListens] = s;
                         parser->ArgusListens++;
+                        MUTEX_UNLOCK(&parser->lock);
 
                         output->ArgusLfd[output->ArgusListens] = s;
                         output->ArgusLfdVersion[output->ArgusListens] = version;
@@ -379,10 +381,12 @@ ArgusEstablishListen (struct ArgusParserStruct *parser,
             setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
             if (!(bind (s, (struct sockaddr *)&sin, sizeof(sin)))) {
                if ((listen (s, ARGUS_LISTEN_BACKLOG)) >= 0) {
+                  MUTEX_LOCK(&parser->lock);
                   parser->ArgusOutputs[parser->ArgusListens] = output;
                   parser->ArgusLfdVersion[parser->ArgusListens] = version;
                   parser->ArgusLfd[parser->ArgusListens] = s;
                   parser->ArgusListens++;
+                  MUTEX_UNLOCK(&parser->lock);
 
                   output->ArgusLfd[output->ArgusListens] = s;
                   output->ArgusLfdVersion[output->ArgusListens] = version;
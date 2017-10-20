openargus/argus-clients-5.0

commit 9bc24c0e17f2452da5d033bcea0b1970dae77314

radhcp: interval tree timers
Remove lease info from interval tree 1 day after expiration.
Introduce garbage collection to free interval tree timers.

examples/radhcp/rabootp_proto_timers.c
@@ -26,13 +26,63 @@
  */
 
 #include <stdlib.h>
+#include <syslog.h>
 #include "argus_threads.h"
+#include "argus_util.h"    /* needed by argus_client.h */
+#include "argus_client.h"  /* ArgusLog() */
 #include "rabootp.h"
 #include "rabootp_memory.h"
 #include "rabootp_client_tree.h"
+#include "rabootp_interval_tree.h"
 
 static const time_t RABOOTP_PROTO_TIMER_NONLEASE=10;
 static const time_t RABOOTP_PROTO_TIMER_HOLDDOWN=30;
+static const time_t RABOOTP_PROTO_TIMER_INTVLTREE=86400;
+static const time_t RABOOTP_PROTO_TIMER_GC=10;
+
+static const unsigned RABOOTP_PROTO_TIMER_GC_MAX = 128;
+static struct argus_timer **gcarray;
+static unsigned gcarraylen = 0;
+static struct argus_timer *gctimer;
+
+
+static ArgusTimerResult
+__gctimer_cb(struct argus_timer *tim, struct timespec *ts)
+{
+   /* A "garbage collector" to free expired timers.  It's not safe
+    * to free a timer from it's own expiration-callback because the
+    * structure is referenced again by the timer library.
+    */
+
+   unsigned count = 0;
+   struct timespec exp = {
+      .tv_sec = RABOOTP_PROTO_TIMER_GC,
+   };
+
+   if (gcarraylen)
+      DEBUGLOG(2, "%s removing %u\n", __func__, gcarraylen);
+
+   while (count < gcarraylen) {
+      free(*(gcarray+count));
+      count++;
+   }
+   gcarraylen = 0;
+   tim->expiry = exp;
+   return RESCHEDULE_REL;
+}
+
+static void
+__gc_schedule(struct argus_timer *tim)
+{
+   if (gcarraylen < RABOOTP_PROTO_TIMER_GC_MAX) {
+      *(gcarray+gcarraylen) = tim;
+      gcarraylen++;
+   } else {
+      /* free one to make room */
+      free(*gcarray);
+      *gcarray = tim;
+   }
+}
 
 /* This function should only be called from the timer thread */
 static ArgusTimerResult
@@ -68,6 +118,42 @@ __lease_exp_cb(struct argus_timer *tim, struct timespec *ts)
    return result;
 }
 
+/* This function should only be called from the timer thread */
+static ArgusTimerResult
+__intvl_exp_cb(struct argus_timer *tim, struct timespec *ts)
+{
+   /* TODO: Don't call mutex functions here.  Add work item to queue
+    * for a "bottom half" thread to handle so we don't block the timer
+    * thread.
+    */
+   struct ArgusDhcpIntvlNode *intvlnode = tim->data;
+   struct ArgusDhcpStruct *ads;
+
+   if (intvlnode == NULL)
+      return FINISHED;
+
+   ads = intvlnode->data;
+   if (ads == NULL)
+      goto cleanup;
+
+   MUTEX_LOCK(ads->lock);
+   if (ads->timers.intvl) {
+      if (tim == ads->timers.intvl)
+         ads->timers.intvl = NULL;
+   }
+   MUTEX_UNLOCK(ads->lock);
+
+   /* decrement refcount -- interval tree is done with this. */
+   RabootpIntvlRemove(&intvlnode->intlo);
+   ArgusDhcpStructFree(ads);
+
+cleanup:
+   ArgusFree(intvlnode);
+   __gc_schedule(tim);
+
+   return FINISHED;
+}
+
 /* This function changes the contents of the cached transaction and should
  * only be called from the main/parse thread
  *
@@ -84,20 +170,34 @@ RabootpProtoTimersLeaseSet(const void * const v_parsed,
 {
    const struct ArgusDhcpStruct * const parsed = v_parsed;
    struct ArgusDhcpStruct *cached = v_cached;
+   struct ArgusDhcpIntvlNode *intvlnode;
 
    /* did we just transition to the BOUND state? */
    if (parsed->state == BOUND && cached->state != BOUND) {
       struct RabootpTimerStruct *rts = v_arg;
-      struct timespec exp = {
+      struct timespec exp_lease = {
          .tv_sec = parsed->rep.leasetime,
       };
+      struct timespec exp_intvl = {
+         .tv_sec = parsed->rep.leasetime + RABOOTP_PROTO_TIMER_INTVLTREE,
+      };
 
-      ArgusDhcpStructUpRef(cached);
       cached->flags &= ~ARGUS_DHCP_LEASEEXP;
       if (cached->timers.lease)
           RabootpTimerStop(rts, cached->timers.lease);
-      cached->timers.lease = RabootpTimerStart(rts, &exp, __lease_exp_cb,
+      else
+         ArgusDhcpStructUpRef(cached); /* up once for the client tree */
+      cached->timers.lease = RabootpTimerStart(rts, &exp_lease, __lease_exp_cb,
                                                cached);
+
+      intvlnode = ArgusCalloc(1, sizeof(*intvlnode));
+      if (intvlnode) {
+         ArgusDhcpStructUpRef(cached); /* up again for the interval tree */
+         intvlnode->data = cached;
+         intvlnode->intlo = cached->last_bind;
+         cached->timers.intvl = RabootpTimerStart(rts, &exp_intvl, __intvl_exp_cb,
+                                                  intvlnode);
+      }
    }
 
    return 0;
@@ -197,17 +297,34 @@ static int RabootpProtoTimersNonleaseSet(const void * const v_parsed,
 
 void RabootpProtoTimersInit(struct RabootpTimerStruct *rts)
 {
+   unsigned const gclen = sizeof(*gcarray) * RABOOTP_PROTO_TIMER_GC_MAX;
+   struct timespec exp = {
+      .tv_sec = RABOOTP_PROTO_TIMER_GC,
+   };
+
    RabootpCallbackRegister(CALLBACK_STATECHANGE,
                            RabootpProtoTimersLeaseSet, rts);
    RabootpCallbackRegister(CALLBACK_XIDUPDATE,
                            RabootpProtoTimersNonleaseSet, rts);
    RabootpCallbackRegister(CALLBACK_XIDNEW,
                            RabootpProtoTimersNonleaseSet, rts);
+
+   gcarray = ArgusMallocAligned(gclen, 64);
+   if (gcarray == NULL)
+      ArgusLog(LOG_ERR, "%s: Unable to allocate garbage collection array\n",
+               __func__);
+
+   gctimer = RabootpTimerStart(rts, &exp, __gctimer_cb, NULL);
 }
 
-void RabootpProtoTimersCleanup(void)
+void RabootpProtoTimersCleanup(struct RabootpTimerStruct *rts)
 {
    RabootpCallbackUnregister(CALLBACK_STATECHANGE, RabootpProtoTimersLeaseSet);
    RabootpCallbackUnregister(CALLBACK_XIDUPDATE, RabootpProtoTimersNonleaseSet);
    RabootpCallbackUnregister(CALLBACK_XIDNEW, RabootpProtoTimersNonleaseSet);
+   RabootpTimerStop(rts, gctimer);
+   ArgusFree(gcarray);
+   gcarray = NULL;
+   free(gctimer);
+   gctimer = NULL;
 }

examples/radhcp/rabootp.c
@@ -1265,6 +1265,9 @@ void RabootpIntvlTreeDump(void)
    IntvlTreeDump(&interval_tree);
 }
 
+/* cached lock must be held by caller.
+ * Caller must also have a reference (incremented refcount) to cached.
+ */
 static int
 __rabootp_update_interval_tree(const void * const v_parsed,
                                void *v_cached,
@@ -1279,6 +1282,7 @@ __rabootp_update_interval_tree(const void * const v_parsed,
          struct ArgusDhcpIntvlTree *head = v_arg;
          struct ArgusDhcpIntvlNode *node;
 
+         /* IntvlTreeFind() ups the refcount on cached if found */
          node = IntvlTreeFind(&interval_tree, &cached->last_bind);
          if (node == NULL) {
             ArgusDhcpIntvlTreeInsert(head,
@@ -1344,6 +1348,11 @@ RabootpCallbackUnregister(enum rabootp_callback_trigger trigger,
 int RabootpClientRemove(struct ArgusDhcpStruct *ads)
 {
    return ArgusDhcpClientTreeRemove(&client_tree, ads);
+}
+
+int RabootpIntvlRemove(const struct timeval * const intlo)
+{
+   return ArgusDhcpIntvlTreeRemove(&interval_tree, intlo);
 }
 
 void

examples/radhcp/rabootp.h
@@ -176,8 +176,9 @@ struct ArgusDhcpV4RequstOptsStruct {
 };
 
 struct ArgusDhcpV4Timers {
-   void *lease;
-   void *non_lease;     /* DHCPDISCOVER, DHCPREQUEST, DHCPRENEW */
+   void *lease;                    /* protocol lease expiry */
+   void *non_lease;                /* out-of-lease timer */
+   void *intvl;                    /* when to remove from interval tree */
 };
 
 enum ArgusDhcpStructFlags {
@@ -217,7 +218,6 @@ struct ArgusDhcpStruct {
    unsigned short num_responders;  /* how many unique servers replied */
    unsigned short total_requests;  /* request packets with this chaddr+xid */
    unsigned short total_unknownops;/* unknown opcodes received */
-   /* ... uint8_t pad[8] ... */
 };
 
 void RabootpCleanup(void);
@@ -226,6 +226,7 @@ ArgusParseDhcpRecord(struct ArgusParserStruct *, struct ArgusRecordStruct *,
                      struct RabootpTimerStruct *);
 
 int RabootpClientRemove(struct ArgusDhcpStruct *);
+int RabootpIntvlRemove(const struct timeval * const intlo);
 void RabootpDumpTree(void);
 char *RabootpDumpTreeStr(int);
 void RabootpIntvlTreeDump(void);

examples/radhcp/rabootp_proto_timers.h
@@ -22,6 +22,6 @@
 # include "rabootp.h"
 
 void RabootpProtoTimersInit(struct RabootpTimerStruct *);
-void RabootpProtoTimersCleanup(void);
+void RabootpProtoTimersCleanup(struct RabootpTimerStruct *);
 
 #endif

examples/radhcp/radhcp.c
@@ -317,7 +317,7 @@ RaParseComplete (int sig)
          mysql_close(RaMySQL);
 #endif
          pthread_join(timer_thread, NULL);
-         RabootpProtoTimersCleanup();
+         RabootpProtoTimersCleanup(timer);
          RabootpTimerCleanup(timer);
          RabootpCallbacksCleanup();
          RabootpCleanup();

commit 762a80492e7ec7cd03f8e4a1a8f27625e14a6ee0

radhcp: store time of first bind in intvl tree node
The interval tree is always indexed by the time that a lease was first
aquired.  Always use this first time even if the lease is renewed.
The interval-tree timer was using time of last bind, which of course
works once.

Fixes: 9bc24c0e17f2 ("radhcp: interval tree timers")

examples/radhcp/rabootp_proto_timers.c
@@ -213,7 +213,7 @@ RabootpProtoTimersLeaseSet(const void * const v_parsed,
             ArgusDhcpStructUpRef(cached);
          }
          intvlnode->data = cached;
-         intvlnode->intlo = cached->last_bind;
+         intvlnode->intlo = cached->first_bind;
          cached->timers.intvl = RabootpTimerStart(rts, &exp_intvl, __intvl_exp_cb,
                                                   intvlnode);
       }
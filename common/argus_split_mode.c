openargus/argus-clients-5.0

commit 1a32428f8aa7eb1b0e7a9bc547e3bdc1e5cb72f5

rasplit.h: make RaSplitModes static const
RaSplitModes is declared and defined in a header file and never modified
by any client.  Remove the ifdef'ed extern and just make this static
const.

include/rasplit.h
@@ -77,9 +77,7 @@ extern "C" {
 #define ARGUSSPLITMINUTE	6
 #define ARGUSSPLITSECOND	7
 
-#if defined(ArgusClient)
-
-char *RaSplitModes[ARGUSSPLITMODENUM] = {
+static const char *RaSplitModes[ARGUSSPLITMODENUM] = {
   "time",
   "count",  
   "size",    
@@ -92,12 +90,6 @@ char *RaSplitModes[ARGUSSPLITMODENUM] = {
   "rate",    
 };
 
-#else
-
-extern char *RaSplitModes[ARGUSSPLITMODENUM];
-
-#endif
- 
 char *RaSplitFilename (struct ArgusAdjustStruct *);
  
 extern void ArgusAlignConfig(struct ArgusParserStruct *, struct ArgusAdjustStruct *);

commit d2f1251b525e862858c68072a4732ed0898280ba

move RaSplitModes array to argus_split_mode.c
Defining the array as static in a header file generates compiler warnings
for source files that include rasplit.h but do not use the array.
Export the array from the argus_client library instead.

Fixes: 1a32428f8aa7 ("rasplit.h: make RaSplitModes static const")

common/argus_split_mode.c
@@ -3,6 +3,19 @@
 #include "argus_client.h"
 #include "argus_sort.h"
 
+const char *RaSplitModes[ARGUSSPLITMODENUM] = {
+  "time",
+  "count",  
+  "size",    
+  "flow",    
+  "pattern",    
+  "nomodify",    
+  "hard",    
+  "soft",    
+  "zero",    
+  "rate",    
+};
+
 /* Since the mode pointer may be advanced here, return the resulting
  * value.  If the split mode is either ARGUSSPLITSIZE or ARGUSSPLITCOUNT,
  * it is up to the caller to truncate the ArgusSorter array.

include/rasplit.h
@@ -77,18 +77,7 @@ extern "C" {
 #define ARGUSSPLITMINUTE	6
 #define ARGUSSPLITSECOND	7
 
-static const char *RaSplitModes[ARGUSSPLITMODENUM] = {
-  "time",
-  "count",  
-  "size",    
-  "flow",    
-  "pattern",    
-  "nomodify",    
-  "hard",    
-  "soft",    
-  "zero",    
-  "rate",    
-};
+const char *RaSplitModes[ARGUSSPLITMODENUM];
 
 char *RaSplitFilename (struct ArgusAdjustStruct *);
  
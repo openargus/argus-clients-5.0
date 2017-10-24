openargus/argus-clients-5.0

commit ca1009777377ae65199a8f41a1e37e684f09d98b

radhcp: update interval tree
Extend the interval for the interval-tree node representing a lease
that has been renewed.  Remember both the time of the first lease and
the time of the last renewal.

examples/radhcp/rabootp_interval_tree.c
@@ -106,6 +106,8 @@ ArgusDhcpIntvlTreeInsert(struct ArgusDhcpIntvlTree *head,
 
    node = ArgusCalloc(1, sizeof(*node));
    if (node) {
+      struct ArgusDhcpIntvlNode *exist;
+
       node->data = ads;
       node->intlo = *start;
       node->inthi = *start;
@@ -113,7 +115,14 @@ ArgusDhcpIntvlTreeInsert(struct ArgusDhcpIntvlTree *head,
       node->subtreehi = node->inthi;
 
       MUTEX_LOCK(&head->lock);
-      if (RB_INSERT(dhcp_intvl_tree, &head->inttree, node)) {
+      exist = RB_INSERT(dhcp_intvl_tree, &head->inttree, node);
+      if (exist != NULL) {
+         /* extend this node's interval */
+         exist->inthi.tv_sec = ads->last_bind.tv_sec + seconds;
+         if (timercmp(&exist->subtreehi, &exist->inthi, <))
+            exist->subtreehi = exist->inthi;
+
+         /* clean up the node we allocated but don't need */
          ArgusFree(node);
          node = NULL;
       } else {

examples/radhcp/rabootp.c
@@ -189,13 +189,12 @@ __parse_one_dhcp_record(const struct ether_header * const ehdr,
       newstate = fsa_advance_state(&parsed, ads);
       if (ads->state != newstate) {
          if (newstate == BOUND) {
-
-            /* DEBUG -- THIS CONDITIONAL "FIXES" THE INDEFINITE REFCOUNT */
-            if (ads->last_bind.tv_sec == 0) {
+            if (ads->first_bind.tv_sec == 0) {
+               ads->first_bind.tv_sec = time->end.tv_sec;
+               ads->first_bind.tv_usec = time->end.tv_usec;
+            }
             ads->last_bind.tv_sec = time->end.tv_sec;
             ads->last_bind.tv_usec = time->end.tv_usec;
-            }
-
          }
          parsed.state = newstate;
          rabootp_cb_exec(&callback.state_change, &parsed, ads);
@@ -1328,12 +1327,13 @@ __rabootp_update_interval_tree(const void * const v_parsed,
 
          ArgusDhcpStructUpRef(cached);
          if (ArgusDhcpIntvlTreeInsert(head,
-                                      &cached->last_bind,
+                                      &cached->first_bind,
                                       parsed->rep.leasetime,
-                                      cached) != 0) {
+                                      cached) != 0)
+            /* interval found for this transaction and was updated.
+             * Do not increment the refcount again.
+             */
             ArgusDhcpStructFree(cached);
-            DEBUGLOG(1, "CAN'T UPDATE INTERVAL TREE YET!!!\n");
-         }
       }
    }
    return 0;

examples/radhcp/rabootp.h
@@ -222,10 +222,11 @@ struct ArgusDhcpStruct {
    /* fourth + fifth cachelines */
    struct ArgusDhcpV4LeaseOptsStruct rep; /* This is a linked list of replies */
 
-   /* sixth cacheline */
-   struct ArgusDhcpV4Timers timers;
+   /* sixth+ cacheline */
    struct timeval first_req;       /* this client transaction was first seen */
+   struct timeval first_bind;      /* first time we entered the BOUND state */
    struct timeval last_bind;       /* last time we entered the BOUND state */
+   struct ArgusDhcpV4Timers timers;
 
 };
 

examples/radhcp/rabootp_patricia_tree.c
@@ -101,14 +101,13 @@ RabootpPatriciaTreeUpdate(const struct ArgusDhcpStruct * const parsed,
 
    ArgusDhcpStructUpRef(cached);
    if (ArgusDhcpIntvlTreeInsert(intvltree,
-                                &cached->last_bind,
+                                &cached->first_bind,
                                 parsed->rep.leasetime,
-                                cached) != 0) {
-      /* can't update interval tree yet.  we already complain about this
-       * elsewhere
+                                cached) != 0)
+      /* interval found for this transaction and was updated.
+       * Do not increment the refcount again.
        */
       ArgusDhcpStructFree(cached);
-   }
 
 out:
    return rv;

commit 3582603024fe451300c82f01eca7e6c2915e3b7f

update interval-tree subtreehi on node update
When a lease is extended, the corresponding node in the interval tree
must be updated to reflect the lease's new end time.  This can change
the max value for interval end times in that subtree.  When that
changes the new value must be checked against that of parent nodes
since they may also need to be updated.

Fixes: ca1009777377 ("radhcp: update interval tree")

examples/radhcp/rabootp_interval_tree.c
@@ -126,7 +126,7 @@ ArgusDhcpIntvlTreeInsert(struct ArgusDhcpIntvlTree *head,
          /* extend this node's interval */
          exist->inthi.tv_sec = ads->last_bind.tv_sec + seconds;
          if (timercmp(&exist->subtreehi, &exist->inthi, <))
-            exist->subtreehi = exist->inthi;
+            __dhcp_intvl_node_update(exist);
 
          /* clean up the node we allocated but don't need */
          ArgusFree(node);
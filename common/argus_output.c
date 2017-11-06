openargus/argus-clients-5.0

commit 7e0b322d8ad8034172b7289c229d9266b8e1ab91

argus_output.c: respond to client commands quickly
Processing client commands from either a client argus data socket or from
a control socket is split between two threads: ArgusListenProcess() and
__ArgusOutputProcess().  The listening thread marks the client readable
when select() returns and includes that client's file descriptor in the
"readfds" fd_set.  It also removes the file descriptor from the readfds
fd_set.  In response, the output thread reads the command, responds and
then marks the client unreadable.  Now the listening thread will again
select() on that file descriptor.

Since the listener's select times out after one second, noticing the next
client command can take up to one second.  Add a pair of pipe(2) file
descriptors to each output (ArgusOutputStruct) that the output thread can
use to signal the listener when it is done processing a client command.
One fd of each pair will also be added to the listener's select so that
it wakes up in a timely fashion.

common/argus_output.c
@@ -507,6 +507,7 @@ ArgusNewOutput (struct ArgusParserStruct *parser, int sasl_min_ssf,
    retn->sasl_min_ssf = sasl_min_ssf;
    retn->sasl_max_ssf = sasl_max_ssf;
    retn->auth_localhost = auth_localhost;
+   retn->ListenNotify[0] = retn->ListenNotify[1] = -1;
 
    ArgusInitOutput(retn);
 
@@ -533,6 +534,10 @@ ArgusDeleteOutput (struct ArgusParserStruct *parser, struct ArgusOutputStruct *o
       ArgusFree(output->ArgusInitMar);
 
    parser->ArgusOutputList = NULL;
+   if (output->ListenNotify[0] >= 0)
+      close(output->ListenNotify[0]);
+   if (output->ListenNotify[1] >= 0)
+      close(output->ListenNotify[1]);
    ArgusFree(output);
 
 
@@ -3357,6 +3362,7 @@ static int
 __build_output_array(struct ArgusParserStruct *parser,
                      struct ArgusOutputStruct *outputs[],
                      int lfd[],
+                     int notifyfd[], /* pipes for completion notification */
                      char lfdver[])
 {
    int i;
@@ -3368,6 +3374,7 @@ __build_output_array(struct ArgusParserStruct *parser,
          outputs[count] = parser->ArgusOutputs[i];
          lfd[count] = parser->ArgusLfd[i];
          lfdver[count] = parser->ArgusLfdVersion[i];
+         notifyfd[count] = parser->ArgusOutputs[i]->ListenNotify[0];
          count++;
       }
    }
@@ -3388,6 +3395,7 @@ void *ArgusListenProcess(void *arg)
    struct ArgusOutputStruct *output;
    struct ArgusOutputStruct *outputs[ARGUS_MAXLISTEN];
    int lfd[ARGUS_MAXLISTEN];
+   int notifyfd[ARGUS_MAXLISTEN];
    char lfdver[ARGUS_MAXLISTEN];
    int nbout;
 
@@ -3402,7 +3410,7 @@ void *ArgusListenProcess(void *arg)
    if (arg == NULL)
       goto out;
 
-   nbout = __build_output_array(parser, outputs, lfd, lfdver);
+   nbout = __build_output_array(parser, outputs, lfd, notifyfd, lfdver);
 
 #if defined(ARGUS_THREADS)
    sigfillset(&blocked_signals);
@@ -3432,6 +3440,10 @@ void *ArgusListenProcess(void *arg)
                FD_SET(lfd[cur], &readmask);
                width = (lfd[cur] > width) ? lfd[cur] : width;
             }
+            if (notifyfd[cur] != -1) {
+               FD_SET(notifyfd[cur], &readmask);
+               width = (notifyfd[cur] > width) ? notifyfd[cur] : width;
+            }
 
             output = outputs[cur];
 
@@ -3457,6 +3469,7 @@ void *ArgusListenProcess(void *arg)
          if ((val = select (width + 1, &readmask, NULL, NULL, &wait)) >= 0) {
             if (val > 0) {
                struct ArgusClientData *client;
+               char pchar;
 
 #ifdef ARGUSDEBUG
                ArgusDebug(3, "%s() select returned with tasks\n", __func__);
@@ -3466,6 +3479,9 @@ void *ArgusListenProcess(void *arg)
 
                   if (FD_ISSET(lfd[cur], &readmask))
                      ArgusCheckClientStatus(outputs[cur], lfd[cur], lfdver[cur]);
+
+                  if (FD_ISSET(notifyfd[cur], &readmask))
+                     read(notifyfd[cur], &pchar, 1);
 
                   output = outputs[cur];
 
@@ -3489,7 +3505,7 @@ void *ArgusListenProcess(void *arg)
          }
 
 #if defined(ARGUS_THREADS)
-      nbout = __build_output_array(parser, outputs, lfd, lfdver);
+      nbout = __build_output_array(parser, outputs, lfd, notifyfd, lfdver);
 #endif
    }
 
@@ -3528,6 +3544,14 @@ __ArgusOutputProcess(struct ArgusOutputStruct *output,
 #if defined(ARGUS_THREADS)
    sigfillset(&blocked_signals);
    pthread_sigmask(SIG_BLOCK, &blocked_signals, NULL);
+   if (pipe(output->ListenNotify) < 0) {
+      /* If the fd is -1, write() will fail with EBADF which we will
+       * ignore.  The result will be slower responses to client commands.
+       */
+      ArgusLog(LOG_WARNING, "%s/%s: unable to open pipe fd pair\n", caller,
+               __func__);
+      output->ListenNotify[0] = output->ListenNotify[1] = -1;
+   }
 
    while (!(output->status & ARGUS_STOP)) {
 #else
@@ -3753,6 +3777,9 @@ __ArgusOutputProcess(struct ArgusOutputStruct *output,
                         if (checkmessage(output, client) < 0)
                            delete = 1;
                         client->readable = 0;
+
+                        /* tell ArgusListenProcess() we're done */
+                        write(output->ListenNotify[1], "0", 1);
                      }
 
                      if (ArgusWriteOutSocket (output, client) < 0) {

include/argus_output.h
@@ -130,6 +130,9 @@ struct ArgusOutputStruct {
 #if defined(ARGUS_THREADS)
    pthread_t thread;
    pthread_mutex_t lock;
+   int ListenNotify[2]; /* used by output thread to signal the listen thread
+                         * that it is finished reading the client's command
+                         */
 #endif
 
    struct ArgusSourceStruct *ArgusSrc;

commit 290a6ab94906077c6a293686d8e5adeb61aba927

argus_output.c: init,close ListenNotify for control channels
Do the same initialization and cleanup of the ArgusOutputStruct's
ListenNotify[] array in ArgusNewControlChannel and
ArgusDeleteControlChannel that is done in ArgusNewOutput and
ArgusDeleteOutput.

Fixes: 7e0b322d8ad8 ("argus_output.c: respond to client commands quickly")

common/argus_output.c
@@ -591,6 +591,9 @@ ArgusNewControlChannel (struct ArgusParserStruct *parser)
       retn->ArgusReportTime.tv_usec -= 1000000;
    }
 
+#if defined(ARGUS_THREADS)
+   retn->ListenNotify[0] = retn->ListenNotify[1] = -1;
+#endif
    ArgusInitControlChannel(retn);
 
 #ifdef ARGUSDEBUG
@@ -615,6 +618,12 @@ ArgusDeleteControlChannel (struct ArgusParserStruct *parser, struct ArgusOutputS
    if (output->ArgusInitMar != NULL)
       ArgusFree(output->ArgusInitMar);
    parser->ArgusOutputList = NULL;
+#if defined(ARGUS_THREADS)
+   if (output->ListenNotify[0] >= 0)
+      close(output->ListenNotify[0]);
+   if (output->ListenNotify[1] >= 0)
+      close(output->ListenNotify[1]);
+#endif
    ArgusFree(output);
 
 #ifdef ARGUSDEBUG
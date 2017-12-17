b619483c4d9f0b7cba1041cf051d912c69430dc7
Eric Kinzie <eric@qosient.com>
2017-12-17 17:00:25 -0500

    rapath: fix compiler warning for type punned deref
    Null terminate string without casting to an integer.

--- examples/rapath/rapath.c
+++ examples/rapath/rapath.c
@@ -1009,7 +1009,7 @@ RaSendArgusRecord(struct ArgusRecordStruct *argus)
                   ArgusParser->Lflag = 0;
             }
 
-            *(int *)&buf = 0;
+            buf[0] = 0;
             ArgusPrintRecord(ArgusParser, buf, argus, MAXSTRLEN);
             
             if (ArgusParser->Aflag && (!(RaPrintAddr))) {

4eb3921864095dec50edb474a66467c782aaf715
Carter Bullard <carter@qosient.com>
2018-03-22 17:48:59 -0400

    Account for ns score value when merging records.

--- common/argus_client.c
+++ common/argus_client.c
@@ -8298,6 +8298,9 @@ ArgusMergeRecords (struct ArgusAggregatorStruct *na, struct ArgusRecordStruct *n
                   }
                }
 
+               if (ns1->score > ns2->score)
+                  ns1->score = ns2->score;
+
                ns1->status |= ARGUS_RECORD_MODIFIED;
                break;
             }

openargus/argus-clients-5.0

commit 8bcc6f4ca91045ddaaf59a12c036c96b2dd4cf35

radhcp: allow search for address prefixes
A query string can now contain a netmask in the addr= field.  For
example
  SEARCH: when=2017/02/15+1d,addr=192.168.8.0/24,pullup

Reverse the notion of left and right in the patricia  tree traversal
functions to match the order of data stored in the tree.  Stop tree
traveral when the next node is the parent of the subtree root.

Trac ticket #124

examples/radhcp/radhcp.c
@@ -137,6 +137,59 @@ __is_oneshot_query(void)
    return 0;
 }
 
+static int
+__parse_ipv4_prefix(const char * const prefixstr, struct in_addr *addr,
+                    unsigned char *masklen)
+{
+   char *cpy = strdup(prefixstr);
+   char *saveptr;
+   char *tok;
+   int rv = 0;
+   unsigned long val;
+
+   if (cpy == NULL)
+      ArgusLog(LOG_ERR, "%s unable to allocate string\n", __func__);
+
+   tok = strtok_r(cpy, "/", &saveptr);
+   if (tok == NULL) {
+      rv = -1;
+      goto out;
+   }
+
+   if (inet_aton(tok, addr) != 1) {
+      rv = -1;
+      goto out;
+   }
+
+   tok = strtok_r(NULL, "/", &saveptr);
+   if (tok == NULL) {
+      /* no slash is ok.  we're done */
+      *masklen = 32;
+      goto out;
+   }
+
+   errno = 0;    /* To distinguish success/failure after call */
+   val = strtoul(tok, &saveptr, 10);
+   if ((errno == ERANGE && (val == ULONG_MAX || val == 0))
+        || (errno != 0 && val == 0)) {
+      DEBUGLOG(4, "%s: strtoul returned an error: %s\n", __func__,
+               strerror(errno));
+      rv = -1;
+      goto out;
+   }
+
+   if (saveptr == tok) {
+      DEBUGLOG(4, "%s: strtoul found no digits\n", __func__);
+      rv = -1;
+   }
+
+   *masklen = (unsigned char)(val & 0xff);
+
+out:
+   free(cpy);
+   return rv;
+}
+
 /* SEARCH: <argus-time-string> */
 char **
 ArgusHandleSearchCommand (struct ArgusOutputStruct *output, char *command)
@@ -156,6 +209,7 @@ ArgusHandleSearchCommand (struct ArgusOutputStruct *output, char *command)
    struct timeval starttime_tv;
    struct timeval endtime_tv;
    struct in_addr addr = {0, };
+   unsigned char masklen; /* prefix length */
 
    enum QueryOptsEnum {
       OPT_WHEN = 0,
@@ -214,7 +268,7 @@ ArgusHandleSearchCommand (struct ArgusOutputStruct *output, char *command)
    }
 
    if (parsed[OPT_ADDR]) {
-      if (inet_aton(parsed[OPT_ADDR], &addr) != 1) {
+      if (__parse_ipv4_prefix(parsed[OPT_ADDR], &addr, &masklen) < 0) {
          retn[0] = "Invalid IP address\n";
          retn[1] = "FAIL\n";
          res = -1;
@@ -293,7 +347,8 @@ ArgusHandleSearchCommand (struct ArgusOutputStruct *output, char *command)
    } else {
       tmp_invec_used = 0;
       if (MUTEX_LOCK(&ArgusParser->lock) == 0) {
-         tmp_invec_used = RabootpPatriciaTreeSearch(&addr, &starttime_tv,
+         tmp_invec_used = RabootpPatriciaTreeSearch(&addr, masklen,
+                                                    &starttime_tv,
                                                     &endtime_tv, tmp_invec,
                                                     invec_nitems);
          MUTEX_UNLOCK(&ArgusParser->lock);

examples/radhcp/rabootp.c
@@ -114,7 +114,7 @@ __find_lease_by_addr(const struct dhcp_packet *bp, struct timeval *when)
          return NULL;
 
       ciaddr.s_addr = EXTRACT_32BITS(&bp->ciaddr.s_addr);
-      nitems = RabootpPatriciaTreeSearch(&ciaddr, when, when, invec, 1024);
+      nitems = RabootpPatriciaTreeSearch(&ciaddr, 32, when, when, invec, 1024);
       DEBUGLOG(1, "%s: found %u transaction(s) for IP address 0x%08x\n",
                __func__, nitems, ciaddr.s_addr);
       for (i = 0; i < nitems; i++) {

examples/radhcp/rabootp_patricia_tree.c
@@ -117,7 +117,11 @@ out:
 
 
 /* __minmax and __next adapted from BSD's sys/tree.h for non-recursive
- * traversal
+ * traversal.  The argus patricia trees are "backwards" in that the
+ * the left subtree holds values (addresses) numerically greater
+ * than the parent node and the right subtree holds values less
+ * than the parent node, so reverse the minmax and next functions
+ * so to reflect that.
  */
 
 static
@@ -129,30 +133,35 @@ __minmax(struct RaAddressStruct *elm, int val)
    while (tmp) {
       parent = tmp;
       if (val < 0)
-         tmp = tmp->l;
-      else
          tmp = tmp->r;
+      else
+         tmp = tmp->l;
    }
    return (parent);
 }
 
 static
 struct RaAddressStruct *
-__next(struct RaAddressStruct *elm)
+__next(struct RaAddressStruct *elm, struct RaAddressStruct *subtreeroot)
 {
    if (elm == NULL)
       return NULL;
 
-   if (elm->r) {
-      elm = elm->r;
-      while (elm->l)
-         elm = elm->l;
+   if (elm->l) {
+      elm = elm->l;
+      while (elm->r)
+         elm = elm->r;
    } else {
-      if (elm->p && elm == (elm->p)->l)
+      if (elm->p && elm == (elm->p)->r) {
+         if (elm == subtreeroot)
+            return NULL;
          elm = elm->p;
-      else {
-         while (elm->p && elm == (elm->p)->r)
+      } else {
+         while (elm->p && elm == (elm->p)->l) {
             elm = elm->p;
+            if (elm == subtreeroot)
+               return NULL;
+         }
          elm = elm->p;
       }
    }
@@ -168,14 +177,18 @@ RabootpPatriciaTreeForeach(struct RaAddressStruct *node,
    struct RaAddressStruct *tmp;
    int rv = 0;
 
-   for (tmp = __minmax(node, -1); tmp && !rv; tmp = __next(tmp))
+   for (tmp = __minmax(node, -1); tmp && !rv; tmp = __next(tmp, node))
       rv = cb(tmp, arg);
+
+   if (rv == 0)
+      rv = cb(node, arg);
 
    return rv;
 }
 
 static struct RaAddressStruct *
 __find(const unsigned int * const yiaddr,
+       unsigned char masklen,
        struct ArgusParserStruct *parser)
 {
    struct ArgusLabelerStruct *labeler;
@@ -185,18 +198,19 @@ __find(const unsigned int * const yiaddr,
 
    memset(&node, 0, sizeof(node));
    node.addr.addr[0] = *yiaddr;
-   node.addr.masklen = 32;
-   node.addr.mask[0] = 0xFFFFFFFF;
+   node.addr.masklen = masklen;
+   node.addr.mask[0] = 0xFFFFFFFF << (32 - masklen);
 
    return RaFindAddress(parser, labeler->ArgusAddrTree[AF_INET], &node,
-                        ARGUS_EXACT_MATCH);
+                        masklen == 32 ? ARGUS_EXACT_MATCH : ARGUS_MASK_MATCH);
 }
 
 struct RaAddressStruct *
 RabootpPatriciaTreeFind(const unsigned int * const yiaddr,
+                        unsigned char masklen,
                         struct ArgusParserStruct *parser)
 {
-   return __find(yiaddr, parser ? parser : ArgusParser);
+   return __find(yiaddr, masklen, parser ? parser : ArgusParser);
 }
 
 /* remove one lease from the patricia tree */
@@ -216,7 +230,7 @@ RabootpPatriciaTreeRemoveLease(const unsigned int * const yiaddr,
    if (parser == NULL)
       parser = ArgusParser;
 
-   ras = __find(yiaddr, parser);
+   ras = __find(yiaddr, 32, parser);
    if (ras == NULL) {
       rv = -1;
       goto out;
@@ -281,6 +295,7 @@ __search_ipaddr_cb(struct rabootp_l2addr_entry *e, void *arg)
 /* caller must hold ArgusParser lock */
 int
 RabootpPatriciaTreeSearch(const struct in_addr * const addr,
+                          unsigned char masklen,
                           const struct timeval * const starttime,
                           const struct timeval * const endtime,
                           struct ArgusDhcpIntvlNode *invec,
@@ -291,7 +306,7 @@ RabootpPatriciaTreeSearch(const struct in_addr * const addr,
    struct invecStruct x;
    int rv = 0;
 
-   ras = RabootpPatriciaTreeFind(&addr->s_addr, ArgusParser);
+   ras = RabootpPatriciaTreeFind(&addr->s_addr, masklen, ArgusParser);
    if (ras == NULL)
      goto out;
 
@@ -306,8 +321,17 @@ RabootpPatriciaTreeSearch(const struct in_addr * const addr,
    itr.starttime = starttime;
    itr.endtime = endtime;
 
-   if (ras->obj)
-      rabootp_l2addr_list_foreach(ras->obj, __search_ipaddr_cb, &itr);
+   if (masklen != 32) {
+      struct RaAddressStruct *tmp;
+
+      for (tmp = __minmax(ras, -1); tmp; tmp = __next(tmp, ras)) {
+         if (tmp->obj)
+            rabootp_l2addr_list_foreach(tmp->obj, __search_ipaddr_cb, &itr);
+      }
+   } else {
+      if (ras->obj)
+         rabootp_l2addr_list_foreach(ras->obj, __search_ipaddr_cb, &itr);
+   }
 
    rv = (int)x.used;
 
@@ -372,7 +396,7 @@ RabootpPatriciaTreeDump(struct RaAddressStruct *ras, char *s, size_t slen)
     *
     */
 
-   for (tmp = __minmax(ras, -1); tmp && !rv; tmp = __next(tmp))
+   for (tmp = __minmax(ras, -1); tmp && !rv; tmp = __next(tmp, ras))
       rv = __display_ipv4_cb(tmp, &string);
 
    return (ssize_t)string.len;

examples/radhcp/rabootp_patricia_tree.h
@@ -37,6 +37,7 @@ RabootpPatriciaTreeForeach(struct RaAddressStruct *,
 
 struct RaAddressStruct *
 RabootpPatriciaTreeFind(const unsigned int * const,
+                        unsigned char masklen,
                         struct ArgusParserStruct *);
 
 int
@@ -49,6 +50,7 @@ RabootpPatriciaTreeRemoveLease(const unsigned int * const,
 
 int
 RabootpPatriciaTreeSearch(const struct in_addr * const,
+                          unsigned char masklen,
                           const struct timeval * const,
                           const struct timeval * const,
                           struct ArgusDhcpIntvlNode *,

commit 1b1eec6a86d19b1a2c924a2d96d5870b8691376e

radhcp: fixed uninitialized variable compiler warning
./radhcp.c:350:52: warning: ‘masklen’ may be used uninitialized in this function [-Wmaybe-uninitialized]
          tmp_invec_used = RabootpPatriciaTreeSearch(&addr, masklen,

Fixes: 8bcc6f4ca910 ("radhcp: allow search for address prefixes")

examples/radhcp/radhcp.c
@@ -209,7 +209,7 @@ ArgusHandleSearchCommand (struct ArgusOutputStruct *output, char *command)
    struct timeval starttime_tv;
    struct timeval endtime_tv;
    struct in_addr addr = {0, };
-   unsigned char masklen; /* prefix length */
+   unsigned char masklen = 32; /* prefix length */
 
    enum QueryOptsEnum {
       OPT_WHEN = 0,
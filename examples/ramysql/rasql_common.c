openargus/argus-clients-5.0

commit 479e81be5686098711506877849124dbe0956ed2

add function to get SQL column names and primary key
RasqlManageGetColumns() retreives an array of column names and sorts
the array so that the columns that make up the primary key are first.
The output parameter keylen indicates how many of the array elements
make up the key.  The return value is the total number of column names
in the array.

examples/ramysql/rasql_common.c
@@ -327,3 +327,87 @@ RaSQLQueryTable (MYSQL *RaMySQL, const char **tables,
    ArgusFree(sbuf);
    ArgusFree(ArgusSQLStatement);
 }
+
+/* The array *columns[] must be allocated to hold ncolumns number of
+ * char *pointers, but the char* elements should not be allocated before
+ * calling RasqlManageGetColumns().  On return, the first *keylen elements
+ * in the array are primary keys according to SQL.  The element following
+ * the last column name is always NULL, so really only ncolumns-1 column
+ * names can be recorded.
+ */
+int
+RasqlManageGetColumns(MYSQL *RaMySQL, const char * const table, char **columns,
+                      size_t ncolumns, size_t *keylen)
+{
+   unsigned int num_fields;
+   unsigned int i;
+   unsigned int pricount;	/* primary key columns */
+   unsigned int other;		/* non-key column index */
+   int retn = -1;
+   int slen;
+   char *query;
+   MYSQL_RES *result;
+   MYSQL_FIELD *fields;
+
+
+   query = ArgusMalloc(MAXSTRLEN);
+   if (query == NULL)
+      ArgusLog(LOG_ERR, "%s unable to allocate query buffer\n", __func__);
+
+   slen = snprintf(query, MAXSTRLEN, "SELECT * from %s LIMIT 1", table);
+   if (slen >= MAXSTRLEN) {
+#ifdef ARGUSDEBUG
+      ArgusDebug(4, "%s query string too long\n", __func__);
+#endif
+   }
+
+   retn = mysql_real_query(RaMySQL, query, slen);
+   if (retn) {
+      if (mysql_errno(RaMySQL) == ER_NO_SUCH_TABLE)
+         retn = 0;
+#ifdef ARGUSDEBUG
+      ArgusDebug(4, "mysql_real_query error %s", mysql_error(RaMySQL));
+#endif
+      goto out;
+   }
+
+   result = mysql_store_result(RaMySQL);
+   if (result == NULL)
+      goto out;
+
+   num_fields = mysql_num_fields(result);
+   if (num_fields > ncolumns) {
+#ifdef ARGUSDEBUG
+      ArgusDebug(4, "%s not enough space to store column names\n", __func__);
+#endif
+      goto out;
+   }
+
+   fields = mysql_fetch_fields(result);
+   pricount = 0;
+   other = num_fields;
+
+   /* Build the array of column names from both ends -- primary key
+    * columns have the lowest numbered indices, non-key columns have
+    * the largest indices.
+    */
+
+   for (i = 0; i < num_fields && pricount < other; i++) {
+      if (fields[i].flags & PRI_KEY_FLAG) {
+         columns[pricount] = strdup(fields[i].name);
+         pricount++;
+      } else {
+         other--;
+         columns[other] = strdup(fields[i].name);
+      }
+   }
+   if (num_fields < ncolumns)
+      columns[num_fields] = NULL;
+
+   retn = (int)num_fields;
+   *keylen = pricount;
+
+out:
+   ArgusFree(query);
+   return retn;
+}

examples/ramysql/rasql_common.h
@@ -26,4 +26,6 @@ ArgusCreateSQLTimeTableNames (struct ArgusParserStruct *parser,
 void
 RaSQLQueryTable (MYSQL *, const char **, int, int, const char **); 
 
+int
+RasqlManageGetColumns(MYSQL *, const char * const, char **, size_t , size_t *);
 #endif

commit 1038348e9d3a0b4aca896e405c40e02ae6ffc065

rename function RasqlManageGetColumns for style consistency
Change the name to RaSQLManageGetColumns to match other sql-related
functions.

Fixes: 479e81be5686 ("add function to get SQL column names and primary key")

examples/ramysql/rasql_common.c
@@ -330,13 +330,13 @@ RaSQLQueryTable (MYSQL *RaMySQL, const char **tables,
 
 /* The array *columns[] must be allocated to hold ncolumns number of
  * char *pointers, but the char* elements should not be allocated before
- * calling RasqlManageGetColumns().  On return, the first *keylen elements
+ * calling RaSQLManageGetColumns().  On return, the first *keylen elements
  * in the array are primary keys according to SQL.  The element following
  * the last column name is always NULL, so really only ncolumns-1 column
  * names can be recorded.
  */
 int
-RasqlManageGetColumns(MYSQL *RaMySQL, const char * const table, char **columns,
+RaSQLManageGetColumns(MYSQL *RaMySQL, const char * const table, char **columns,
                       size_t ncolumns, size_t *keylen)
 {
    unsigned int num_fields;

examples/ramysql/rasql_common.h
@@ -27,7 +27,7 @@ void
 RaSQLQueryTable (MYSQL *, const char **, int, int, const char **); 
 
 int
-RasqlManageGetColumns(MYSQL *, const char * const, char **, size_t , size_t *);
+RaSQLManageGetColumns(MYSQL *, const char * const, char **, size_t , size_t *);
 
 void
 RaSQLOptimizeTables (MYSQL *, const char **);

examples/ramysql/rasqlinsert.c
@@ -423,7 +423,7 @@ main(int argc, char **argv)
                if (!strcmp("Seconds", RaTables[i]))
                   continue;
 
-               done = RasqlManageGetColumns(RaMySQL+1, RaTables[i], ArgusTableColumnName,
+               done = RaSQLManageGetColumns(RaMySQL+1, RaTables[i], ArgusTableColumnName,
                       sizeof(ArgusTableColumnName)/sizeof(ArgusTableColumnName[0]),
                       &ArgusTableColumnKeys);
             }
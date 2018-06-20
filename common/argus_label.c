a6764c6aa42432d762b09edd1129797c8a5a98f4
Carter Bullard <carter@qosient.com>
2018-06-20 12:37:39 -0400

    fix bug in patricia tree routines for ARGUS_EXACT_MATCH
    ARGUS_EXACT_MATCH would match on node matches if an intermediate
    node matched that didn't have any children.  This is OK for, but
    for exact matches (search for a child node as an example).
    Feature is important, so fix is to add another mode.

--- common/argus_label.c
+++ common/argus_label.c
@@ -1247,16 +1247,13 @@ RaFindAddress (struct ArgusParserStruct *parser, struct RaAddressStruct *tree, s
 
                   case ARGUS_EXACT_MATCH: 
                      if ((node->addr.masklen == tree->addr.masklen) &&
-                         (node->addr.addr[0] == tree->addr.addr[0])) 
+                         (node->addr.addr[0] == tree->addr.addr[0]))
                         retn = tree;
                      else
-                     if ((tree->l == NULL) && (tree->r == NULL)) {
-                        retn = tree;
-                     } else
                      if (tree->l || tree->r) {
                         if ((node->addr.addr[0] >> (32 - (tree->addr.masklen + 1))) & 0x01)
                           retn = RaFindAddress (parser, tree->l, node, mode);
-                        else 
+                        else
                           retn = RaFindAddress (parser, tree->r, node, mode);
                      }
                      done++;

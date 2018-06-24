10e2b43c07a1da33b05975e84c5dbe33bb6e0185
Carter Bullard <carter@qosient.com>
2018-06-24 11:41:18 -0400

    argus_label.h: add missing function declarations
    Add declarations for RaInsertAddressTree and RaInsertLocalityTree.

--- include/argus_label.h
+++ include/argus_label.h
@@ -231,6 +231,9 @@ void RaPrintLabelTree (struct ArgusLabelerStruct *, struct RaAddressStruct *, in
 struct RaAddressStruct *RaFindAddress (struct ArgusParserStruct *, struct RaAddressStruct *, struct RaAddressStruct *, int);
 struct RaAddressStruct *RaInsertAddress (struct ArgusParserStruct *, struct ArgusLabelerStruct *, struct RaAddressStruct *, struct RaAddressStruct *, int);
 
+int RaInsertAddressTree (struct ArgusParserStruct *, struct ArgusLabelerStruct *labeler, char *);
+int RaInsertLocalityTree (struct ArgusParserStruct *, struct ArgusLabelerStruct *labeler, char *);
+
 char *RaPruneAddressTree (struct ArgusLabelerStruct *, struct RaAddressStruct *, int, int);
 
 void RaLabelMaskAddressStatus(struct RaAddressStruct *, unsigned int);
@@ -286,6 +289,10 @@ extern int ArgusAddToRecordLabel (struct ArgusParserStruct *, struct ArgusRecord
 
 extern struct RaAddressStruct *RaFindAddress (struct ArgusParserStruct *, struct RaAddressStruct *, struct RaAddressStruct *, int);
 extern struct RaAddressStruct *RaInsertAddress (struct ArgusParserStruct *, struct ArgusLabelerStruct *, struct RaAddressStruct *, struct RaAddressStruct *, int);
+
+extern int RaInsertAddressTree (struct ArgusParserStruct *, struct ArgusLabelerStruct *labeler, char *);
+extern int RaInsertLocalityTree (struct ArgusParserStruct *, struct ArgusLabelerStruct *labeler, char *);
+
 extern char *RaPruneAddressTree (struct ArgusLabelerStruct *, struct RaAddressStruct *, int, int);
 
 extern void RaLabelMaskAddressStatus(struct RaAddressStruct *, unsigned int);

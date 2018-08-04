4133722ee327af030786d78461739bd471d417e8
Carter Bullard <carter@qosient.com>
2018-08-04 00:29:37 -0400

    Add ArgusFetchEtherType to support rahisto style statistics.

--- common/argus_client.c
+++ common/argus_client.c
@@ -12298,6 +12298,19 @@ ArgusFetchDstAddr (struct ArgusRecordStruct *argus)
    return(retn);
 }
 
+
+double
+ArgusFetchEtherType (struct ArgusRecordStruct *ns)
+{
+   struct ArgusMacStruct *m1 = (struct ArgusMacStruct *) ns->dsrs[ARGUS_MAC_INDEX];
+   double retn = 0;
+
+   if (m1)
+      retn = m1->mac.mac_union.ether.ehdr.ether_type;
+
+   return(retn);
+}
+
 double
 ArgusFetchProtocol (struct ArgusRecordStruct *ns)
 {

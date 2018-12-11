openargus/argus-clients-5.0

commit 7b9b31f2cc1c87319a35873247e649b1ea1fc1ed

ramanage: clear curl commandline string for each file
Be sure to reset the string length and remaining byte count each time
a new curl commandline is formatted.  Previously, __upload_init() did
this once before an entire batch of files was uploaded.  Modify that
function to allow it to be called multiple times in the same process
and call once for each file uploaded.

clients/ramanage.c
@@ -431,6 +431,12 @@ __upload_init(CURL **hnd, const configuration_t * const config)
    long auth = 0;
    int slen;
 
+#ifdef HAVE_LIBCURL
+   /* with libcurl we don't need to re-initialize anything */
+   if (*hnd != NULL)
+      return 0;
+#endif
+
    userpwd = ArgusMalloc(NAME_MAX);
    if (userpwd == NULL)
       ArgusLog(LOG_ERR,
@@ -487,13 +493,17 @@ __upload_init(CURL **hnd, const configuration_t * const config)
    if (curlexe == NULL)
       ArgusLog(LOG_ERR, "unable to copy curl executable name\n");
 
-   *hnd = rstr = ArgusMalloc(sizeof(*rstr));
-   if (rstr == NULL)
-      ArgusLog(LOG_ERR, "unable to allocate curl commandline string struct\n");
+   rstr = *hnd;
+   if (rstr == NULL) {
+      *hnd = rstr = ArgusMalloc(sizeof(*rstr));
+      if (rstr == NULL)
+         ArgusLog(LOG_ERR,
+                  "unable to allocate curl commandline string struct\n");
 
-   rstr->str = ArgusMalloc(PATH_MAX);
-   if (rstr == NULL)
-      ArgusLog(LOG_ERR, "unable to allocate curl commandline string\n");
+      rstr->str = ArgusMalloc(PATH_MAX);
+      if (rstr == NULL)
+         ArgusLog(LOG_ERR, "unable to allocate curl commandline string\n");
+   }
 
    rstr->remain = PATH_MAX;
    rstr->len = 0;
@@ -627,7 +637,8 @@ __upload(CURL *hnd, const char * const filename, off_t filesz,
       DEBUGLOG(4, "cmd: %s\n", rstr->str);
       ret = system(rstr->str);
       if (ret > 0) {
-         ArgusLog(LOG_WARNING, "curl command failed, returned %d\n", ret);
+         ArgusLog(LOG_WARNING, "curl command failed, returned %d.  (%s)\n",
+                  rstr->str, ret);
          ret = -ret; /* child process failed */
       }
    }
@@ -945,13 +956,14 @@ RamanageUpload(const struct ArgusParserStruct * const parser,
       return 0;
    }
 
-   if (__upload_init(&hnd, config) < 0) {
-      ArgusLog(LOG_WARNING, "unable to initialize libcurl\n");
-      return -1;
-   }
-
    i = 0;
+   hnd = NULL;
    while (i < filcount && upload_kb <= config->upload_max_kb) {
+      if (__upload_init(&hnd, config) < 0) {
+         ArgusLog(LOG_WARNING, "unable to initialize libcurl\n");
+         return -1;
+      }
+
       file = filvec[i];
       DEBUGLOG(4, "upload file %s size %u\n", file->filename,
                file->statbuf.st_size);

commit 2f42b4dc20c52aba1ac49d1e998982589cb57876

ramanage: fix order or parameters in ArgusLog in __upload
When logging the message "curl command failed, returned . . .", make
sure the integer and string parameters match the order of %s and %d.

Fixes: 7b9b31f2cc1c ("ramanage: clear curl commandline string for each file")

clients/ramanage.c
@@ -1173,7 +1173,7 @@ __upload(CURL *hnd, const char * const filename, off_t filesz,
       ret = system(rstr->str);
       if (ret > 0) {
          ArgusLog(LOG_WARNING, "curl command failed, returned %d.  (%s)\n",
-                  rstr->str, ret);
+                  ret, rstr->str);
          ret = -ret; /* child process failed */
       }
    }
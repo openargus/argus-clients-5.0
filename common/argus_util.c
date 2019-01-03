openargus/argus-clients-5.0

commit 5512db91e1c4fd251ed51120c5d8d9ac27898ab7

RaProcessRecursiveFiles: keep the results of stat()
The ArgusInput structure already has a statbuf.  Since stat() is
called for every file during the recursive file search, copy the
results into the input structure for later use.

common/argus_util.c
@@ -499,6 +499,9 @@ RaProcessRecursiveFiles (char *path)
 #endif
          if (!(ArgusAddFileList (ArgusParser, path, ARGUS_DATA_SOURCE, -1, -1)))
             ArgusLog (LOG_ERR, "error: -R file arg %s\n", path);
+
+         /* Copy the stat() results since we already have them. */
+         ((struct ArgusInput *)ArgusParser->ArgusInputFileListTail)->statbuf = statbuf;
       }
    }
 

commit 7c83ad7970ffbaa96d81f7bc4b04d9d9a7b26321

common: keep the results of stat when building file list
Commit 5512db91e1c4 changed RaProcessRecursiveFiles to copy the buffer
filled in by stat() into the ArgusInputFile structure for the newly
added file.  Applications needing information about the file do not have
to call stat() a second time.

Make the same change in RaDescend().  stat() is called for every file
added to the input list in this function; always keep the results so
that all entries in the list have this info.

ArgusParseArgs() can also add files to the list for the -r option.
Add a call to stat() here.

Fixes: 5512db91e1c4 ("RaProcessRecursiveFiles: keep the results of stat()")

common/argus_util.c
@@ -578,6 +578,10 @@ RaDescend(char *name, size_t len, size_t end)
                      if (!(ArgusAddFileList (ArgusParser, name, ARGUS_DATA_SOURCE, -1, -1)))
                         ArgusLog (LOG_ERR, "error: -R file arg %s\n", name);
 
+                     /* Copy the stat() results since we already have them. */
+                     ((struct ArgusFileInput *)
+                      ArgusParser->ArgusInputFileListTail)->statbuf = statbuf;
+
                      retn++;
                   }
                }
@@ -1581,6 +1585,9 @@ ArgusParseArgs(struct ArgusParserStruct *parser, int argc, char **argv)
 
                      if (!(ArgusAddFileList (parser, optarg, type, ostart, ostop)))
                         ArgusLog(LOG_ERR, "%s: error: file arg %s", *argv, optarg);
+
+                     stat(optarg, &((struct ArgusFileInput *)
+                                    ArgusParser->ArgusInputFileListTail)->statbuf);
 
                      if ((optarg = argv[optind]) != NULL)
                         if (*optarg != '-')
openargus/argus-clients-5.0

commit 1233f04aa446e433e9fb134f4fe5dbaf0f4701c3

ramanage: send libcurl debug output to ArgusDebug
If compiled with debugging support, the libcurl debug output will be
sent to the argus client debug log.  Suppress writing received data
to stdout.

clients/ramanage.c
@@ -843,6 +843,12 @@ typedef ramanage_str_t CURL;
 #endif
 
 #ifdef HAVE_LIBCURL
+static size_t
+RamanageLibcurlWriteCallback(char *ptr, size_t size, size_t nmemb, void *user)
+{
+   return size*nmemb;
+}
+
 static int
 __trace(CURL *handle, curl_infotype type, char *data, size_t size, void *userp)
 {
@@ -853,7 +859,10 @@ __trace(CURL *handle, curl_infotype type, char *data, size_t size, void *userp)
 
    switch (type) {
       case CURLINFO_TEXT:
-         ArgusLog(LOG_INFO, "libcurl: %s", data);
+      case CURLINFO_HEADER_IN:
+      case CURLINFO_HEADER_OUT:
+      case CURLINFO_DATA_IN:
+         DEBUGLOG(2, "libcurl: %s", data);
          break;
       default: /* in case a new one is introduced to shock us */
          break;
@@ -974,7 +983,11 @@ __upload_init(CURL **hnd, const configuration_t * const config)
    curl_easy_setopt(*hnd, CURLOPT_SSL_VERIFYPEER, 0L);
    curl_easy_setopt(*hnd, CURLOPT_SSL_VERIFYHOST, 0L);
    curl_easy_setopt(*hnd, CURLOPT_TCP_KEEPALIVE, 1L);
+   curl_easy_setopt(*hnd, CURLOPT_WRITEFUNCTION, RamanageLibcurlWriteCallback);
+# ifdef ARGUSDEBUG
    curl_easy_setopt(*hnd, CURLOPT_DEBUGFUNCTION, __trace);
+   curl_easy_setopt(*hnd, CURLOPT_VERBOSE, 1L);
+# endif /* ARGUSDEBUG */
 #else	/* HAVE_LIBCURL */
    ramanage_str_t *rstr;
    char * const curlexe =

commit 5d1ac5a83972531ec58510651c1324d8d3ba4a23

ramanage: __trace function only used for debugging
The __trace() function is a callback for libcurl that is only used when
the argus clients are compiled with debugging support.  Fix compiler warning
about unused function by putting this inside of an #ifdef.

Fixes: 1233f04aa446 ("ramanage: send libcurl debug output to ArgusDebug")

clients/ramanage.c
@@ -849,6 +849,7 @@ RamanageLibcurlWriteCallback(char *ptr, size_t size, size_t nmemb, void *user)
    return size*nmemb;
 }
 
+# ifdef ARGUSDEBUG
 static int
 __trace(CURL *handle, curl_infotype type, char *data, size_t size, void *userp)
 {
@@ -870,7 +871,8 @@ __trace(CURL *handle, curl_infotype type, char *data, size_t size, void *userp)
 
   return 0;
 }
-#endif
+# endif /* ARGUSDEBUG */
+#endif /* HAVE_LIBCURL */
 
 /* hash must have 20 bytes allocated */
 static int
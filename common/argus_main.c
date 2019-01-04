openargus/argus-clients-5.0

commit 5247c32301e41a729574ab07a580e07c1a3f01c3

common: defer initializing global name arrays
Do not allocate memory unnecessarily.  Defer initializing name arrays
until needed in ArgusPrintRecord.

common/argus_util.c
@@ -30600,14 +30600,6 @@ ArgusParseInit (struct ArgusParserStruct *parser, struct ArgusInput *input)
       input->ArgusConvPtr = input->ArgusConvBuffer;
    }
 
-   if (!(ArgusParseInited)) {
-      if (input)
-         ArgusInitAddrtoname (parser, input->ArgusLocalNet, input->ArgusNetMask);
-      else
-         ArgusInitAddrtoname (parser, 0L, 0L);
-      ArgusParseInited = 1;
-   }
-
 #ifdef ARGUSDEBUG
    if (input) {
       ArgusDebug (2, "ArgusParseInit(%p %p\n", parser, input);

commit b963146f70690b400a2ad604df55efd8fc2dd912

common: do not use argus record input pointer after free
struct ArgusRecordStruct contains a pointer to the ArgusInput structure
that was the source of the flow record.  When reading files, this pointer
is no longer valid (has been freed) before RaParseComplete() is called.

The input pointer was used by ArgusInitAddrtoname() to get the local
subnet information.  Create a new function, ArgusSetLocalNet(), that
can be called as the files are processed.  Current behavior is preserved
such that the last input determines the local subnet.

Fixes: 5247c32301e4 ("common: defer initializing global name arrays")

common/argus_main.c
@@ -337,6 +337,7 @@ main (int argc, char **argv)
                                */
       }
 
+      ArgusSetLocalNet(input->ArgusLocalNet, input->ArgusNetMask);
       ArgusFree(input);
       input = NULL;
    }

common/argus_util.c
@@ -184,6 +184,7 @@ int RaParseResourceFile (struct ArgusParserStruct *parser, char *file,
                          ResourceCallback cb);
 static int RaParseResourceLine(struct ArgusParserStruct *, int, char *, int, int);
 void setArgusEventDataRecord (struct ArgusParserStruct *, char *);
+static void ArgusInitAddrtoname(struct ArgusParserStruct *);
 
 #define ARGUS_RCITEMS                           80
 
@@ -5310,10 +5311,7 @@ ArgusPrintRecord (struct ArgusParserStruct *parser, char *buf, struct ArgusRecor
    int slen = 0, dlen = len;
 
    if (!(ArgusParseInited)) {
-      if (argus->input)
-         ArgusInitAddrtoname (parser, argus->input->ArgusLocalNet, argus->input->ArgusNetMask);
-      else
-         ArgusInitAddrtoname (parser, 0L, 0L);
+      ArgusInitAddrtoname (parser);
       ArgusParseInited = 1;
    }
    if (parser->ArgusPrintJson) {
@@ -22087,17 +22085,19 @@ ArgusInitDSCodepointarray()
  * of the local network.  mask is its subnet mask.
  */
 
-void ArgusInitAddrtoname(struct ArgusParserStruct *, u_int, u_int);
-
 void
-ArgusInitAddrtoname(struct ArgusParserStruct *parser, u_int localnet, u_int mask)
+ArgusSetLocalNet(u_int localnet, u_int mask)
 {
-   netmask = mask;
-   if (parser->fflag) {
-      f_localnet = localnet;
-      f_netmask = mask;
+   if (ArgusParser->fflag) {
+       f_localnet = localnet;
+       f_netmask = mask;
+       netmask = mask;
    }
+}
 
+static void
+ArgusInitAddrtoname(struct ArgusParserStruct *parser)
+{
    if (ArgusEtherArrayInited == 0)
       ArgusInitEtherarray();
 
@@ -22112,7 +22112,7 @@ ArgusInitAddrtoname(struct ArgusParserStruct *parser, u_int localnet, u_int mask
    ArgusInitDSCodepointarray();
 
 #ifdef ARGUSDEBUG
-   ArgusDebug (1, "ArgusInitAddrtoname (%p, 0x%x, 0x%x)\n", parser, localnet, mask);
+   ArgusDebug (1, "ArgusInitAddrtoname (%p)\n", parser);
 #endif
 }
 

include/argus_util.h
@@ -1476,7 +1476,6 @@ struct ArgusTokenStruct llcsap_db[] = {
 };
 
 void ArgusLoadList(struct ArgusListStruct *, struct ArgusListStruct *);
-void ArgusInitAddrtoname(struct ArgusParserStruct *, u_int, u_int);
 
 void ArgusInitServarray(struct ArgusParserStruct *);
 void ArgusInitEprotoarray(void);
@@ -1488,6 +1487,8 @@ void ArgusFreeServarray(struct ArgusParserStruct *);
 void ArgusFreeProtoidarray(void);
 void ArgusFreeEtherarray(void);
 void ArgusFreeLlcsaparray(void);
+void ArgusSetLocalNet(u_int localnet, u_int mask);
+
 
 unsigned int ArgusIndexRecord (struct ArgusRecordStruct *);
 
@@ -1832,7 +1833,6 @@ extern void ArgusProcessLabelOptions(struct ArgusParserStruct *, char *);
 extern void ArgusProcessGroupOptions(struct ArgusParserStruct *, char *);
 
 extern void ArgusLoadList(struct ArgusListStruct *, struct ArgusListStruct *);
-extern void ArgusInitAddrtoname(struct ArgusParserStruct *, u_int, u_int);
 
 extern void ArgusInitServarray(struct ArgusParserStruct *);
 extern void ArgusInitEprotoarray(void);
@@ -1844,6 +1844,7 @@ extern void ArgusFreeServarray(struct ArgusParserStruct *);
 extern void ArgusFreeProtoidarray(void);
 extern void ArgusFreeEtherarray(void);
 extern void ArgusFreeLlcsaparray(void);
+extern void ArgusSetLocalNet(u_int localnet, u_int mask);
 
 extern char *ip_proto_string [];
 extern char *icmptypestr[];
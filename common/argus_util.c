4b9cd91c1ccb7b785b9b5172c3fad2d9d6e17690
Eric Kinzie <eric@qosient.com>
2019-11-24 13:55:10 -0500

    argus_util: fix null deref parsing RA_FIELD_DELIMITER
    If strchr() fails to find a single-quote in the option string,
    set the string pointer (ptr) to the original option string
    and continue parsing from there.

--- common/argus_util.c
+++ common/argus_util.c
@@ -1944,11 +1944,12 @@ RaParseResourceLine(struct ArgusParserStruct *parser, int linenum,
          break;
 
       case RA_FIELD_DELIMITER:
-         ptr = optarg;
          if ((ptr = strchr (optarg, '\'')) != NULL) {
             ptr++;
             if (ptr[0] == '\'')
                break;
+         } else {
+            ptr = optarg;
          }
 
          if (ptr[0] == '\\') {

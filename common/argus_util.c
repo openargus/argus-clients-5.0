de55ef6ca2ae781770ad9590053663bad58a1201
Carter Bullard <carter@qosient.com>
2020-07-03 15:49:04 -0400

    Fix multiple closings of pipe when zipped ...

--- common/argus_util.c
+++ common/argus_util.c
@@ -28577,6 +28577,7 @@ ArgusReadConnection (struct ArgusParserStruct *parser, struct ArgusInput *input,
                            ArgusDebug (1, "ArgusReadConnection() read %d bytes from pipe\n", cnt);
 #endif
                            input->file = input->pipe;
+                           input->pipe = NULL;
                         }
                      }
 

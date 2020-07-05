465a221f21179093118225b126e7406ed500424e
Carter Bullard <carter@qosient.com>
2020-07-05 09:04:39 -0400

    Mods to deal with failure to read more than one compressed files.
    Fix change to ArgusCloseInput strategy, don't clear pipe, test first
    and do pclose() and clear file.

--- common/argus_util.c
+++ common/argus_util.c
@@ -28577,7 +28577,6 @@ ArgusReadConnection (struct ArgusParserStruct *parser, struct ArgusInput *input,
                            ArgusDebug (1, "ArgusReadConnection() read %d bytes from pipe\n", cnt);
 #endif
                            input->file = input->pipe;
-                           input->pipe = NULL;
                         }
                      }
 
@@ -29498,14 +29497,14 @@ ArgusCloseInput(struct ArgusParserStruct *parser, struct ArgusInput *input)
 
    input->status |= ARGUS_CLOSED;
 
-   if (input->file) {
-      fclose(input->file);
-      input->file = NULL;
-   }
-
    if (input->pipe) {
       pclose(input->pipe);
       input->pipe = NULL;
+      input->file = NULL;
+   }
+
+   if (input->file) {
+      fclose(input->file);
       input->file = NULL;
    }
 
